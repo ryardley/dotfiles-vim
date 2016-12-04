77a8491c22450356ee1b28affa717e3ccf2bbb7d
Rudi Yardley <contact@rudiyardley.com>
2016-12-05 06:57:23 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,4 @@
-WD=$(pwd)
-
 # install ycm
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh --tern-completer
-cd $WD
 
-if [ "$(uname)" == "Darwin" ]; then 
-  if [ -z "$(which brew)" ]; then
-    brew install ag
-  fi
-else
-  echo "Please install ag"
-fi
